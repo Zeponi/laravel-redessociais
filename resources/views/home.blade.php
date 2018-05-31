5a023a230eb58a0f137d57668ac8681fa327fb90
Augusto Miguel Zeponi <augustozeponi@hotmail.com>
2018-05-31 16:09:41 -0300

    #320 - Login com Facebook

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,23 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <p>Dados do Usuário:</p>
+                    <p>Nome: {{ $user->name }}</p>
+                    <p>E-mail: {{ $user->email }}</p>
+                    <p>Github: {{ $user->github }}</p>
+                    <p>Facebook: {{ $user->facebook }}</p>
+                    <br>
+                    @if($user->github)
+                        <a class="btn btn-default disabled">Conectado com o Github</a>
+                    @else
+                        <a href="{{ url('/entrar/github') }}" class="btn btn-primary">Conectar com o Github</a>
+                    @endif
+
+                    @if($user->facebook)
+                        <a class="btn btn-default disabled">Conectado com o Facebook</a>
+                    @else
+                        <a href="{{ url('/entrar/facebook') }}" class="btn btn-primary">Conectar com o Facebook</a>
+                    @endif
                 </div>
             </div>
         </div>
